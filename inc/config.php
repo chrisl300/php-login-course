a22a3a69a51462ffe12ffe6caadba583588524a2
Chris Lewis <chrlewis+git@gmail.com>
2017-10-31 21:52:22 +0000

    Pre Ajax save

---
+++ inc/config.php
@@ -0,0 +1,12 @@
+<?php
+
+  //If there is no constant defined called __CONFIG__, do not load this Files
+  // eg. if user came directly here.
+  if(!defined('__CONFIG__')){
+    // Should really show 404 error page or something
+    exit('You do not have a config file');
+  }
+
+  // Our config is below
+  
+?>
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+  //allow the config
+  define('__CONFIG__', true);
+  //require the config
+  require_once "inc/config.php";
+  ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -15,37 +22,13 @@
   <body>
 
     <div class="uk-section uk-container">
-      <div class="uk-grid uk-child-width-1-3@s .uk-child-width-1-1" uk-grid>
-        <form class="uk-form-stacked" js-login>
-
-          <div class="uk-margin">
-              <label class="uk-form-label" for="form-stacked-text">Email</label>
-              <div class="uk-form-controls">
-                  <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
-              </div>
-          </div>
-
-          <div class="uk-margin">
-              <label class="uk-form-label" for="form-stacked-text">Password</label>
-              <div class="uk-form-controls">
-                  <input class="uk-input" id="form-stacked-text" type="password" required='required' laceholder="Your Password">
-              </div>
-          </div>
-              <div class="uk-margin">
-                  <button class="uk-button uk-button-default" type="submit">Login</button>
-
-              </div>
-        </form>
+        <?php
+          echo "Hello world. Today is: ";
+          echo date("Y m d");
+          ?>
     </div>
 
-
-
-    <!-- jQuery is required -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-    <!-- UIkit JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>
+    <?php require_once "inc/footer.php"; ?>
 
   </body>
 </html>
